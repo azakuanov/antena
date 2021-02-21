ef21233f2a027ba2f56215345ea5f64f014f039a
azakuanov <azat.zakuanov@thrivemarket.com>
2021-02-21 13:56:07 -0800

    first commit

---
+++ core/src/main/java/de/danoeh/antennapod/core/feed/util/ImageResourceUtils.java
@@ -0,0 +1,61 @@
+package de.danoeh.antennapod.core.feed.util;
+
+import de.danoeh.antennapod.core.asynctask.ImageResource;
+import de.danoeh.antennapod.core.feed.FeedItem;
+import de.danoeh.antennapod.core.feed.FeedMedia;
+import de.danoeh.antennapod.core.preferences.UserPreferences;
+
+/**
+ * Utility class to use the appropriate image resource based on {@link UserPreferences}
+ */
+public final class ImageResourceUtils {
+
+    private ImageResourceUtils() {
+    }
+
+    /**
+     * returns the image location, does prefer the episode cover if available.
+     */
+    public static String getEpisodeImageLocation(ImageResource resource) {
+        return resource.getImageLocation();
+    }
+
+
+    /**
+     * returns the image location, does prefer the episode cover if available and enabled in settings.
+     */
+    public static String getEpisodeListImageLocation(ImageResource resource) {
+
+        if (UserPreferences.getUseEpisodeCoverSetting()) {
+            return resource.getImageLocation();
+        } else {
+            return getShowImageLocation(resource);
+        }
+    }
+
+    public static String getFallbackImageLocation(ImageResource resource) {
+        return getShowImageLocation(resource);
+    }
+
+    private static String getShowImageLocation(ImageResource resource) {
+
+        if (resource instanceof FeedItem) {
+            FeedItem item = (FeedItem) resource;
+            if (item.getFeed() != null) {
+                return item.getFeed().getImageLocation();
+            } else {
+                return null;
+            }
+        } else if (resource instanceof FeedMedia) {
+            FeedMedia media = (FeedMedia) resource;
+            FeedItem item = media.getItem();
+            if (item != null && item.getFeed() != null) {
+                return item.getFeed().getImageLocation();
+            } else {
+                return null;
+            }
+        } else {
+            return resource.getImageLocation();
+        }
+    }
+}
