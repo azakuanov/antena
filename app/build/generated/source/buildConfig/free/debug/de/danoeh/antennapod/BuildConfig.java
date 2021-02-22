fbfa1c8d7c0ea09383939dbf2ea34fd0d9c1cb49
Azat <azat.zakuanov@yahoo.com>
2021-02-21 17:48:23 -0800

    second commit

---
+++ app/build/generated/source/buildConfig/free/debug/de/danoeh/antennapod/BuildConfig.java
@@ -0,0 +1,17 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package de.danoeh.antennapod;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "de.danoeh.antennapod.debug";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "free";
+  public static final int VERSION_CODE = 2010395;
+  public static final String VERSION_NAME = "2.1.3";
+  // Fields from default config.
+  public static final String COMMIT_HASH = "ef21233";
+  public static final String PODCASTINDEX_API_KEY = "XTMMQGA2YZ4WJUBYY4HK";
+  public static final String PODCASTINDEX_API_SECRET = "XAaAhk4^2YBsTE33vdbwbZNj82ZRLABDDqFdKe7x";
+}
---
+++ ui/app-start-intent/build/generated/source/buildConfig/debug/de/danoeh/antennapod/ui/appstartintent/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package de.danoeh.antennapod.ui.appstartintent;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "de.danoeh.antennapod.ui.appstartintent";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "de.danoeh.antennapod.ui.appstartintent";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
